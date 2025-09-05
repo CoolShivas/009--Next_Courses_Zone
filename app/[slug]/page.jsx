e4adff2361b0be5a8e546d1675feaf96ff564fc9
CoolShivas <shivas2710cool00@gmail.com>
2025-09-06 01:10:02 +0530

    Use of Dynamic Route to Explore Courses of CourseList.

---
+++ app/[slug]/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <center>Dynamic Routes.</center>;
+};
+
+export default page;
--- app/components/CourseList.jsx
+++ app/components/CourseList.jsx
@@ -2,52 +2,53 @@
 
 import React from "react";
 import { courses, courseDetails } from "@/data";
+import Link from "next/link";
 
 const CourseList = () => {
   //   console.log("Printing the Courses list are :-", courses);
   //   console.log("Printing the CourseDetails list are :-", courseDetails);
   return (
-    <>
-      <center className="m-5">
-        <h3>Available courses on our platform are listed below.</h3>
-      </center>
-      <div>
-        <div className="container my-5 d-flex flex-column justify-center">
-          <div className="row w-100 justify-content-center">
-            {courses.map((curElem) => {
-              return (
-                <div key={curElem.id} className="col-md-4 mb-4">
-                  <div
-                    className="card p-4 text-center"
-                    style={{
-                      backgroundColor: "#2F2F3F",
-                      borderRadius: "12px",
-                      maxWidth: "300px",
-                      width: "100%",
-                      border: "1px solid rgba(255, 255, 255, 0.1",
-                      boxShadow: "0px 4px 8px rgba(0,0,0,0.2)",
-                      cursor: "pointer",
-                      transition: "all 0.3s ease",
-                    }}
-                    onMouseEnter={(e) =>
-                      (e.currentTarget.style.transform = "translateY(-5px)")
-                    }
-                    onMouseLeave={(e) =>
-                      (e.currentTarget.style.transform = "translateY(0)")
-                    }
-                  >
-                    <div className="display-4 mb-3 text-light">
-                      {curElem.icon}
-                    </div>
-                    <div className="card-title text-light">{curElem.title}</div>
-                  </div>
-                </div>
-              );
-            })}
-          </div>
+    <div>
+      <h1 className="text-center text-light my-3">
+        Available courses on our platform are listed below.
+      </h1>
+      <div className="container my-5 d-flex flex-column justify-content-center align-items-center">
+        <div className="row w-100 justify-content-center">
+          {courses.map((curElem) => (
+            <div key={curElem.id} className="col-md-4 mb-4">
+              <div
+                className="card p-4 text-center"
+                style={{
+                  backgroundColor: "#2F2F3F",
+                  borderRadius: "12px",
+                  maxWidth: "300px",
+                  width: "100%",
+                  border: "1px solid rgba(255,255,255,0.1)",
+                  transition: "all 0.3s ease",
+                  boxShadow: "0px 4px 8px rgba(0,0,0,0.2)",
+                  cursor: "pointer",
+                }}
+                onMouseEnter={(e) =>
+                  (e.currentTarget.style.transform = "translateY(-5px)")
+                }
+                onMouseLeave={(e) =>
+                  (e.currentTarget.style.transform = "translateY(0)")
+                }
+              >
+                <div className="display-4 mb-3 text-light">{curElem.icon}</div>
+                <h5 className="card-title text-light">{curElem.title}</h5>
+                <Link
+                  href={`/${curElem.id}`}
+                  className="btn btn-outline-warning mt-4"
+                >
+                  Explore Course
+                </Link>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
