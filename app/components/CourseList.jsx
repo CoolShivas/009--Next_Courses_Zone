679b35f3986af481403012a46032d12deb1151e8
CoolShivas <shivas2710cool00@gmail.com>
2025-09-06 00:20:36 +0530

    Addition of Bootstrap's link i.e, Include via CDN in main layout's head tag for bootstrap css.

--- app/components/CourseList.jsx
+++ app/components/CourseList.jsx
@@ -2,13 +2,18 @@ import React from "react";
 import { courses, courseDetails } from "@/data";
 
 const CourseList = () => {
-  console.log("Printing the Courses list are :-", courses);
-  console.log("Printing the CourseDetails list are :-", courseDetails);
+  //   console.log("Printing the Courses list are :-", courses);
+  //   console.log("Printing the CourseDetails list are :-", courseDetails);
   return (
     <>
-      <center>
+      <center className="m-5">
         <h3>Available courses on our platform are listed below.</h3>
       </center>
+      <div>
+        <div className="container my-5 d-flex flex-column justify-center">
+          <div className="row w-100 justify-content-center">Hello World</div>
+        </div>
+      </div>
     </>
   );
 };
